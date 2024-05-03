b17367aa2098fe446989db2f8bd82b45406a1be2
Sirikorn Mind <sirikornmind@gmail.com>
2024-05-03 16:35:25 +0700

    update homework

--- src/hooks/usePrimeNumber/usePrimeNumber.ts
+++ src/hooks/usePrimeNumber/usePrimeNumber.ts
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+
 const usePrimeNumber = () => {
-    return [true, (_: number) => {}, 0] as const
+    const [checkPrime, setCheckPrime] = useState(true);
+    const [latestNumber, setLatestNumber] = useState(0);
+
+    const calculatePrime = (num: number) => {
+        setLatestNumber(num)
+        if (num === 0 || num === 1) {
+            setCheckPrime(false)
+            return false;
+        }
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                setCheckPrime(false);
+                return false;
+            }
+        }
+        setCheckPrime(true)
+        return true;
+    };
+    
+    return [checkPrime, calculatePrime, latestNumber] as const
 }
 
-export default usePrimeNumber
+export default usePrimeNumber;
