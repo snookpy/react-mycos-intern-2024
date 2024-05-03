4bba25fcf2093593b96cc19a48d149dd6f49ab11
poomiputh <poomiputh_b@cmu.ac.th>
2024-05-03 14:47:21 +0700

    Finish hookHomework

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
 import "./App.css"
+import useThaiWin from "./hooks/useThaiWin/useThaiWin"
 
 function App() {
-	const [count, setCount] = useState(0)
+	const [isLogin, enter, exit] = useThaiWin()
+	console.log(isLogin)
 
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Mycos Frontend Internship 2024</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+			<button onClick={enter}>
+				Enter
+			</button>
+			<button onClick={exit}>
+				Exit
+			</button>
 		</>
 	)
 }
--- src/hooks/usePrimeNumber/usePrimeNumber.ts
+++ src/hooks/usePrimeNumber/usePrimeNumber.ts
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react"
+
 const usePrimeNumber = () => {
-    return [true, (_: number) => {}, 0] as const
+    const [isPrime, setIsPrime] = useState<boolean>(false)
+    const [number, setNumber] = useState<number>(0)
+    useEffect(() => {
+        if (number < 2) {
+            setIsPrime(false)
+            return
+        }
+        for (let i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i === 0) {
+                setIsPrime(false)
+                return
+            }
+        }
+        setIsPrime(true)
+    }, [number])
+    return [isPrime, setNumber, number] as const
 }
 
 export default usePrimeNumber
