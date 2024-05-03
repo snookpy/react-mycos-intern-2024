d1a901ef029e4bba42fb404f48708ad724beddb4
Jiranthanin-Supakad <jiranthanin_su@cmu.ac.th>
2024-05-03 16:26:59 +0700

    updated homework Primenumber

--- src/hooks/usePrimeNumber/usePrimeNumber.ts
+++ src/hooks/usePrimeNumber/usePrimeNumber.ts
@@ -1,5 +1,30 @@
+import { useState } from "react"
+
 const usePrimeNumber = () => {
-    return [true, (_: number) => {}, 0] as const
+    const [isPrime, setIsPrime] = useState(true)
+    const [latestNumber, setLatestNumber] = useState(0);
+
+    const calPrime = (num: number): void => {
+        setLatestNumber(num);
+
+        if (num <= 3 && num != 0) {
+            setIsPrime(true);
+        } else if (num % 2 === 0 || num % 3 === 0 || num <= 1) {
+            setIsPrime(false);
+        } else {
+            let i = 5;
+            while (i * i <= num) {
+                if (num % i === 0 || num % (i + 2) === 0) {
+                    setIsPrime(false);
+                }
+                i += 6;
+            }
+        }
+    };
+
+    return [isPrime, calPrime, latestNumber] as const
 }
 
 export default usePrimeNumber
+
+
